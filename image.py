74319ccd255f01f869342179acbaf9c2acdc9e46
Pratheeka P <110621614+prathipsg@users.noreply.github.com>
2024-08-22 22:02:00 +0530

    Add files via upload

---
+++ image.py
@@ -0,0 +1,42 @@
+import requests
+import streamlit as st
+import base64
+
+def get_img_as_base64(file):
+    with open(file,"rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+img = get_img_as_base64("bgimage.webp")
+
+page_bg_img = f"""
+
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image :url("data:image/png;base64,{img}");
+background-size : cover;
+}}
+[data-testid="stHeader"]{{
+background:rgba(0,0,0,0);
+}}
+</style>
+
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+image_bytes = query({
+	"inputs": st.text_input('Enter prompt:'),
+})
+# You can access the image with PIL.Image for example
+import io
+from PIL import Image
+image = Image.open(io.BytesIO(image_bytes))
+
+if st.button('Generate'):
+	st.image(image)
+	
